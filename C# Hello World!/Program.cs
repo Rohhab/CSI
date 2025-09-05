f5c18c0e090b3a10c904974a7cfd771862dca29f
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-05 22:22:33 +0330

    Update console program to use variable and string interpolation

--- C# Hello World!/Program.cs
+++ C# Hello World!/Program.cs
@@ -1,6 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var csInternshipUrl = "https://github.com/cs-internship";
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("This is C# 13.0!");
 Console.WriteLine("Enjoy coding!");
 Console.WriteLine();
 Console.WriteLine("Join CS Internship Program:");
-Console.WriteLine("Check out this: https://github.com/cs-internship");
+Console.WriteLine($"Check out this: {csInternshipUrl}");
