f468988ed03e900e5bcbc5bb427230cb1eff6393
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-05 22:25:01 +0330

    Update the app to use String.Trim() method

--- C# Hello World!/Program.cs
+++ C# Hello World!/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("This is C# 13.0!");
 Console.WriteLine("Enjoy coding!");
 Console.WriteLine();
 Console.WriteLine("Join CS Internship Program:");
-Console.WriteLine($"Check out this: {csInternshipUrl}");
+Console.WriteLine($"Check out this: {csInternshipUrl.Trim()}");
