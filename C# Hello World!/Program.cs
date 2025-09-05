4ac7bba7a13a657131dbe885fe78e641031d3ee5
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-05 21:28:10 +0330

    Add CS Internship Reference

--- C# Hello World!/Program.cs
+++ C# Hello World!/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("This is C# 13.0!");
+Console.WriteLine("Enjoy coding!");
+Console.WriteLine();
+Console.WriteLine("Join CS Internship Program:");
+Console.WriteLine("Check out this: https://github.com/cs-internship");
