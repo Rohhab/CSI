788a6e575e33b363a81485ac9c95af6392900d0f
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-07 22:39:12 +0330

    Add some tuples and play with them

--- Step 1/Tuples and types/Program.cs
+++ Step 1/Tuples and types/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿var pt = (X: 1, Y: 2);
+
+var slope = (double)pt.Y / (double)pt.X;
+Console.WriteLine($"A line from the origin to the point {pt} has a slope of {slope}.");
+
+pt.X += 5;
+Console.WriteLine($"The point is now at {pt}.");
+
+var pt2 = pt with { Y = 10 };
+Console.WriteLine($"The point 'pt2' is at {pt2}.");
+
+Console.WriteLine(pt.CompareTo(pt2));
