7311efdcf4fbe7718b14d36790af0e7f4277993d
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-07 22:59:23 +0330

    Update Program.cs to go without top-level-statements and Add a record

--- Step 1/Tuples and types/Program.cs
+++ Step 1/Tuples and types/Program.cs
@@ -1,12 +1,34 @@
-﻿var pt = (X: 1, Y: 2);
+﻿using System;
 
-var slope = (double)pt.Y / (double)pt.X;
-Console.WriteLine($"A line from the origin to the point {pt} has a slope of {slope}.");
+namespace TuplesAndTypes;
 
-pt.X += 5;
-Console.WriteLine($"The point is now at {pt}.");
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pt = (X: 1, Y: 2);
+        var slope = (double)pt.Y / (double)pt.X;
+        Console.WriteLine($"A line from the origin to the point {pt} has a slope of {slope}.");
 
-var pt2 = pt with { Y = 10 };
-Console.WriteLine($"The point 'pt2' is at {pt2}.");
+        pt.X += 5;
+        Console.WriteLine($"The point is now at {pt}.");
 
-Console.WriteLine(pt.CompareTo(pt2));
+        var pt2 = pt with { Y = 10 };
+        Console.WriteLine($"The point 'pt2' is at {pt2}.");
+        Console.WriteLine(pt.CompareTo(pt2));
+
+        var pt3 = new Point(1, 4);
+        var pt4 = pt3 with { Y = 10 };
+        pt4.Slope();
+    }
+
+    public record Point(int X, int Y)
+    {
+        public double Slope()
+        {
+            double slope = Y / X;
+            Console.WriteLine(slope);
+            return slope;
+        }
+    }
+}
