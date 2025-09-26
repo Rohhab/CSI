aad4d44a14f91601d8a3c30521880fe8aa96e6f5
Rohhab <s.pooria.hosseini@gmail.com>
2025-09-26 21:48:14 +0330

    Comment all except a simple Console.WriteLine

--- Step 1/Step1 Presentation/Program.cs
+++ Step 1/Step1 Presentation/Program.cs
@@ -1,54 +1,56 @@
-﻿using System;
+﻿//using System;
 
-namespace Step1;
+//namespace Step1;
 
-public class TypeMaster
-{
-    public string name = "Pouria";
-    private int age = 33;
-    private bool adult = true;
-}
+//public class TypeMaster
+//{
+//    public string name = "Pouria";
+//#pragma warning disable //showcase directives
+//    private int age = 33;
+//#pragma warning restore
+//    private bool adult = true;
+//}
 
 // namespace Organizer; // one file-scoped namespace is allowed
 
-public class Counter
-{
-    private int count = 1;
-    //private string count = "Three"; //type can contain one definition at a time
+//public class Counter
+//{
+//    private int count = 1;
+//    //private string count = "Three"; //type can contain one definition at a time
 
-    public void Count()
-    {
-        string localPronoun = "We";
-        // count = true; //conversion problem
-        // string count = "Three"; //shadowing
-        Console.WriteLine($"{localPronoun} have greeted {count} times.");
-        count++; //if count is string, it's a bad operand for ++ operator
-    }
-    // localPronoun = "You"; //scope
-}
+//    public void Count()
+//    {
+//        string localPronoun = "We";
+//        // count = true; //conversion problem
+//        // string count = "Three"; //shadowing
+//        Console.WriteLine($"{localPronoun} have greeted {count} times.");
+//        count++; //if count is string, it's a bad operand for ++ operator
+//    }
+//    // localPronoun = "You"; //scope
+//}
 
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        var counter = new Counter();
-        var typeMaster = new TypeMaster();
-        var world = "World!";
+//public static class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        var counter = new Counter();
+//        var world = "World!";
 
-        var secondTypeMaster = new TypeMaster();
-        var anotherTypeMaster = typeMaster;
+        //var typeMaster = new TypeMaster();
+        //var secondTypeMaster = new TypeMaster();
+        //var anotherTypeMaster = typeMaster;
 
-        Console.WriteLine(object.ReferenceEquals(typeMaster, anotherTypeMaster));
-        Console.WriteLine(object.ReferenceEquals(typeMaster, secondTypeMaster));
-        Console.WriteLine(object.ReferenceEquals(secondTypeMaster, anotherTypeMaster));
+        //Console.WriteLine(object.ReferenceEquals(typeMaster, anotherTypeMaster));
+        //Console.WriteLine(object.ReferenceEquals(typeMaster, secondTypeMaster));
+        //Console.WriteLine(object.ReferenceEquals(secondTypeMaster, anotherTypeMaster));
 
-        Console.WriteLine($"Hello, {typeMaster.name}!");
-        counter.Count();
+        //Console.WriteLine($"Hello, {typeMaster.name}!");
+        //counter.Count();
         Console.WriteLine($"Hello to everyone watching this lecture!");
-        counter.Count();
-        Console.WriteLine(string.Format("Hello, {0}", world));
-        counter.Count();
-    }
-}
+//        counter.Count();
+//        Console.WriteLine(string.Format("Hello, {0}", world));
+//        counter.Count();
+//    }
+//}
 
-// PRESS CTRL + K, U TO UNCOMMENT
+////PRESS CTRL + K, U TO UNCOMMENT
